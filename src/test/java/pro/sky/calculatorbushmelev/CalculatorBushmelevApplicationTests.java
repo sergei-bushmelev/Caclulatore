7b2798a260eb5bdd57a1f31929f26e3651f4dc27
bush_ <bushmelevfz@gmail.com>
2024-11-05 22:43:31 +0300

    Initial commit

---
+++ src/test/java/pro/sky/calculatorbushmelev/CalculatorBushmelevApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorbushmelev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorBushmelevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
